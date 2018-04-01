cbbab7c2c3f2d0a142a95157c5865399614ca080
patzhum <patrick.hum@hotmail.com>
2018-03-31 22:04:58 -0400

    :tongue: :sweat_drops: :sweat_drops:

--- Android/app/src/main/java/me/phum/trakr/MainActivity.kt
+++ Android/app/src/main/java/me/phum/trakr/MainActivity.kt
@@ -1,14 +1,24 @@
 package me.phum.trakr
 
+import android.content.Intent
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.package_list_view.*
 import kotlinx.android.synthetic.main.splash_view.*
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import me.phum.trakr.schema.Loc
+import me.phum.trakr.schema.Pack
 
 class MainActivity : AppCompatActivity() {
 
+    var packageListAdapter : PackageAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val mapIntent = Intent(this, MapActivity::class.java)
+
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.splash_view)
@@ -24,15 +34,11 @@ class MainActivity : AppCompatActivity() {
             }
             delay(300)
             runOnUiThread {
-                render()
+                startActivity(mapIntent)
+                finish()
             }
         }
 
     }
 
-    fun render() {
-        setContentView(R.layout.activity_main)
-
-
-    }
 }
